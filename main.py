5213931f59e331173bc561428b87390dcd341d2e
Anoop Sharma <anoopsharma527@gmail.com>
2022-01-01 22:02:57 +0530

    Added fastapi code

---
+++ main.py
@@ -0,0 +1,34 @@
+# Without using Async, await
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+# Code with Async,Await included
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
